15b86241a771ef869496aef44c433b3d4b35eef0
ckwflash <ckwredhorse@gmail.com>
2025-10-09 21:15:04 +0800

    Remove email field from Person and Patient test cases
    - Updated related test classes and utility classes to reflect the removal of email.
    - Adjusted parsing and command handling to exclude email attributes.
    - Ensured all tests pass after the removal of email functionality.

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -20,10 +20,10 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Patient(new Name("Alex Yeoh"), new Phone("87438807"), 
+            new Patient(new Name("Alex Yeoh"), new Phone("87438807"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
                 getTagSet("friends"), new Note("Peanut allergy")),
-            new Patient(new Name("Bernice Yu"), new Phone("99272758"), 
+            new Patient(new Name("Bernice Yu"), new Phone("99272758"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                 getTagSet("colleagues", "friends")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"),
