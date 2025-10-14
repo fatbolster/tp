ebe3bdeecffdf516ee78178f7e42784c01b64922
ckwflash <ckwredhorse@gmail.com>
2025-10-14 14:46:19 +0800

    Remove Note from Patient in AddCommandParser

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -6,17 +6,18 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.text.ParseException;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import javax.swing.text.html.HTML.Tag;
+
+import org.xml.sax.Parser;
+
 import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Address;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Note;
 import seedu.address.model.person.Patient;
 import seedu.address.model.person.Phone;
-import seedu.address.model.tag.Tag;
 /**
  * Parses input arguments and creates a new AddCommand object
  */
@@ -42,7 +43,7 @@ public class AddCommandParser implements Parser<AddCommand> {
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
-        Patient patient = new Patient(name, phone, address, tagList, new Note("Seafood allergy"));
+        Patient patient = new Patient(name, phone, address, tagList);
 
         return new AddCommand(patient);
     }
